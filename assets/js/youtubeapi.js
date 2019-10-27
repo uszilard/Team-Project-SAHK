298da6f8531cc96d9d356caef2c2d74df4b0e59b
Szilard Udroiu <uszilard@gmail.com>
2019-10-27 18:11:33 +0100

    embeded-youtube-video0inpage
    managed to embed the youtube video into the relevant container, search function work

--- assets/js/youtubeapi.js
+++ assets/js/youtubeapi.js
@@ -14,32 +14,41 @@ function displayMusicInfo() { }
 displayMusicInfo();
 //
 
+var youTube = $("#youtube");
 
 //
-function getVideo() {
+function searchByKeyword(searchTerm) {
     $.ajax({
         type: 'GET',
         url: 'https://www.googleapis.com/youtube/v3/search',
         data: {
             key: 'AIzaSyB08uG89n8Ul5LA3j0fu1ubMFmh4SrV44U',
-            q: "cats",
+            q: searchTerm,
             part: 'snippet',
-            maxResults: 1,
+            maxResults: 5,
             type: 'video',
             videoEmbeddable: true,
         },
         success: function (data) {
             embedVideo(data)
+            console.log(data)
         },
         error: function (response) {
             console.log("Request Failed");
         }
     });
 }
+//
 
+
+//
 function embedVideo(data) {
-    $('iframe').attr('src', 'https://www.youtube.com/embed/' + data.items[0].id.videoId)
-    $('h3').text(data.items[0].snippet.title)
-    //$('.description').text(data.items[0].snippet.description)
+    var addVideo = $("<iframe>");
+
+    addVideo.addClass("video-stream");
+
+    addVideo.attr('src', 'https://www.youtube.com/embed/' + data.items[0].id.videoId);
+
+    $("#youtube").append(addVideo);
 }
 //
