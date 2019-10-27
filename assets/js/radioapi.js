d32fec0c072c1ff1f3617f28388137ad6d26f0a3
Szilard Udroiu <uszilard@gmail.com>
2019-10-27 16:23:03 +0100

    deiveded-api-js-files
    hey, I just decidd that it will make our work much easier if we have a seprate js file for each API

---
+++ assets/js/radioapi.js
@@ -0,0 +1,15 @@
+//
+$("#search-btn").on("click", function (event) {
+    event.preventDefault();
+
+    var userSearchInput = $("#user-search").val();
+
+    console.log(userSearchInput);
+
+    searchByKeyword(userSearchInput);
+});
+
+function displayMusicInfo() { }
+
+displayMusicInfo();
+//
