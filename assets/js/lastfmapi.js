1091b6cbfbd04c74bf9b68b61a140c25f59383e7
Hristina Staykova <hristina.staykova@gmail.com>
2019-10-29 23:52:16 +0100

    code adjusments, tbc

--- assets/js/lastfmapi.js
+++ assets/js/lastfmapi.js
@@ -1,41 +1,34 @@
 function searchLastFm(searchTerm) {
-    var lastFmKey = "110c00d4400e2e57a4f2b2bb856c9c7b";
+  var lastFmKey = "110c00d4400e2e57a4f2b2bb856c9c7b";
 
-    var queryURL = "http://ws.audioscrobbler.com/2.0/?method=album.search&album=" + searchTerm + "&api_key=" + lastFmKey + "&format=json&limit=4";
+  var queryURL =
+    "http://ws.audioscrobbler.com/2.0/?method=album.search&album=" +
+    searchTerm +
+    "&api_key=" +
+    lastFmKey +
+    "&format=json&limit=4";
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    $("#lastFm").empty();
+    response.results.albummatches.album.forEach(element => {
+      var lastFm = $("#lastFm");
+      var albumBox = $("<div>");
+      var albumTitle = $("<p>");
+      var albumImage = $("<img>");
 
-        response.results.albummatches.album.forEach((element, i) => {
-            var lastFm = $("#lastFm")
-            var albumBox = $("<div>")
+      albumTitle.text(element.name);
+      albumImage.attr("src", element.image[2]["#text"]);
 
-            var albumTitle = $("<p>")
+      albumBox.append(albumTitle);
+      albumBox.append(albumImage);
 
-            albumTitle.text(element.name)
-
-            var albumImage = $("<img>")
-
-            albumImage.attr("src", element.image[1]["#text"])
-
-            albumBox.append(albumTitle)
-            albumBox.append(albumImage)
-
-            lastFm.append(albumBox)
-
-            var htmlBreak = $("<br>")
-            lastFm.append(htmlBreak)
-            lastFm.append(htmlBreak)
-
-
-
-
-        });
-        console.log("lastFmResponse", response);
+      lastFm.append(albumBox);
     });
+    console.log("lastFmResponse", response);
+  });
 }
 
-
-searchLastFm(searchTerm)
+searchLastFm(searchTerm);
