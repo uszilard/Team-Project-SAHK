cfef1f3ece132d7241800358d556bee737b3fef1
APaulaMiranda <apaulafmiranda@gmail.com>
2019-10-31 19:58:08 +0100

    update-last-fm-document-ready

--- assets/js/lastfmapi.js
+++ assets/js/lastfmapi.js
@@ -1,7 +1,39 @@
+var lastFmKey = "110c00d4400e2e57a4f2b2bb856c9c7b";
+
+
+
+
+$(document).ready(function () {
+
+
+    var queryURL = "http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=rj&api_key=" + lastFmKey + "&format=json";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        $("#lastFm").empty();
+        response.results.albummatches.album.forEach(element => {
+            var lastFm = $("#lastFm");
+            var albumBox = $("<div>");
+            var albumTitle = $("<p>");
+            var albumImage = $("<img>");
+
+            //albumTitle.text(element.name);
+            albumImage.attr("src", element.image[2]["#text"]);
+
+            albumBox.append(albumTitle);
+            albumBox.append(albumImage);
+
+            lastFm.append(albumBox);
+        });
+        console.log("lastFmResponse", response);
+    });
+});
 
 
 function searchLastFmByKeyword(searchTerm) {
-    var lastFmKey = "110c00d4400e2e57a4f2b2bb856c9c7b";
+
 
     var queryURL =
         "http://ws.audioscrobbler.com/2.0/?method=album.search&album=" +
