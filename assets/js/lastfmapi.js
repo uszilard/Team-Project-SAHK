c15f61813a3ae7b7a6529e5e2f87a64bcc8062af
Szilard Udroiu <uszilard@gmail.com>
2019-11-02 13:33:34 +0100

    commit

--- assets/js/lastfmapi.js
+++ assets/js/lastfmapi.js
@@ -3,50 +3,50 @@ var lastFm = $("#lastFm");
 
 //
 function fetchData(queryURL) {
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    $("#lastFm").empty();
-    console.log(response);
-    var result;
-    if (response.topalbums) {
-      result = response.topalbums.album;
-    } else {
-      result = response.results.albummatches.album;
-    }
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        $("#lastFm").empty();
+        console.log(response);
+        var result;
+        if (response.topalbums) {
+            result = response.topalbums.album;
+        } else {
+            result = response.results.albummatches.album;
+        }
 
-    result.forEach(element => {
-      console.log(element);
-      var albumBox = $("<div>").addClass("col");
-      var albumTitle = $("<p>");
-      var albumImage = $("<img>");
+        result.forEach(element => {
+            console.log(element);
+            var albumBox = $("<div>").addClass("col");
+            var albumTitle = $("<p>");
+            var albumImage = $("<img>");
 
-      albumTitle.text(element.name);
-      albumImage.attr("src", element.image[2]["#text"]);
+            albumTitle.text(element.name);
+            albumImage.attr("src", element.image[3]["#text"]);
 
-      albumBox.append(albumImage);
-      albumBox.append(albumTitle);
-      lastFm.append(albumBox);
+            albumBox.append(albumImage);
+            albumBox.append(albumTitle);
+            lastFm.append(albumBox);
+        });
+        console.log("lastFmResponse", response);
     });
-    console.log("lastFmResponse", response);
-  });
 }
 
-$(document).ready(function() {
-  var queryURL =
-    "http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=rj&api_key=" +
-    lastFmKey +
-    "&format=json&limit=6";
-  fetchData(queryURL);
+$(document).ready(function () {
+    var queryURL =
+        "http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=rj&api_key=" +
+        lastFmKey +
+        "&format=json&limit=4";
+    fetchData(queryURL);
 });
 
 function searchLastFmByKeyword(searchTerm) {
-  var queryURL =
-    "http://ws.audioscrobbler.com/2.0/?method=album.search&album=" +
-    searchTerm +
-    "&api_key=" +
-    lastFmKey +
-    "&format=json&limit=6";
-  fetchData(queryURL);
+    var queryURL =
+        "http://ws.audioscrobbler.com/2.0/?method=album.search&album=" +
+        searchTerm +
+        "&api_key=" +
+        lastFmKey +
+        "&format=json&limit=4";
+    fetchData(queryURL);
 }
