882f99ad2322d31c33b7cd7c74f7d97185af46d0
Karan Shrestha <iamkaranshrestha96@gmail.com>
2019-11-02 13:18:33 +0100

    addedfiles

--- assets/js/getimages.js
+++ assets/js/getimages.js
@@ -1,44 +1,20 @@
-function getImagesData(searchTerm) {
-
-    var apiKey = "563492ad6f917000010000011d8a4d59e1104cf883c51b9a5999f5ba";
-    var queryURL = "https://api.pexels.com/v1/search?query=" + searchTerm;
-    $.ajax({
-        type: "GET",
-        url: queryURL,
-        beforeSend: function (request) {
-            request.setRequestHeader("Authorization", apiKey);
-        }
-
-    }).then(function (response) {
-        console.log("i am responding to you", response)
-        $("#carousel-inner").empty()
-
-        response.photos.forEach((photo, i) => {
-
-            var carouselItem = $("<div>")
-            carouselItem.attr("class", "carousel-item")
-
-            var photoBox = $("<img>")
-            photoBox.attr("src", photo.src.medium)
-            photoBox.attr("class", "img-fluid d-block w-100");
-            photoBox.attr("alt", photo.id)
-
-            if (i === 0) {
-                photoBox.attr("class", "img-fluid d-block w-100 active");
-
-            }
-
-            carouselItem.append(photoBox)
-
-            console.log($(".carousel-inner"))
 
 
+var apiKey = "563492ad6f917000010000011d8a4d59e1104cf883c51b9a5999f5ba";
+var queryURL = "https://api.pexels.com/v1/curated?per_page=1&page=1"
 
+$.ajax({
+    type: "GET",
+    url: queryURL,
+    beforeSend: function (request) {
+        request.setRequestHeader("Authorization", apiKey);
+    }
 
+}).then(function (response) {
+    console.log("i am responding to you", response)
 
-            $(".carousel-inner").append(carouselItem)
+    var imageurl = response.photos[0].url;
+    console.log(imageurl)
+    var firstslide = $("#firstslide").attr("src", imageurl);
 
-
-        });
-    });
-};
+});
