adb080eb271b0ce61ff1c77cff1e3f05e6ab06cc
Hristina Staykova <hristina.staykova@gmail.com>
2019-11-02 14:25:10 +0100

    get images in the carousel

--- assets/js/getimages.js
+++ assets/js/getimages.js
@@ -1,20 +1,17 @@
-
-
 var apiKey = "563492ad6f917000010000011d8a4d59e1104cf883c51b9a5999f5ba";
-var queryURL = "https://api.pexels.com/v1/curated?per_page=1&page=1"
+var queryURL = "https://api.pexels.com/v1/curated?per_page=1&page=1";
 
-$.ajax({
+$(document).ready(function() {
+  $.ajax({
     type: "GET",
     url: queryURL,
-    beforeSend: function (request) {
-        request.setRequestHeader("Authorization", apiKey);
+    beforeSend: function(request) {
+      request.setRequestHeader("Authorization", apiKey);
     }
-
-}).then(function (response) {
-    console.log("i am responding to you", response)
-
-    var imageurl = response.photos[0].url;
-    console.log(imageurl)
-    var firstslide = $("#firstslide").attr("src", imageurl);
-
+  }).then(function(response) {
+    console.log("i am responding to you", response);
+    var imageurl = response.photos[0].src.original;
+    console.log(imageurl);
+    $("#first-slide").attr("src", imageurl);
+  });
 });
