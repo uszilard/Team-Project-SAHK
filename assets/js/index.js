21be2cf29f46d925a2eb9df3be46e2d0821edc59
Hristina Staykova <hristina.staykova@gmail.com>
2019-11-05 13:35:42 +0100

    added links on albums section, css adjustments

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,12 @@
-
-
 // Search Function
-$("#search-btn").on("click", function (event) {
-    event.preventDefault();
+$("#search-btn").on("click", function(event) {
+  event.preventDefault();
 
-    var userSearchInput = $("#user-search").val();
+  var userSearchInput = $("#user-search").val();
 
-    console.log(userSearchInput);
-
-    searchYouTubeByKeyword(userSearchInput);
-    searchLastFmByKeyword(userSearchInput);
-    //getImagesData(userSearchInput);
+  searchYouTubeByKeyword(userSearchInput);
+  searchLastFmByKeyword(userSearchInput);
+  displayNewsBySearchWord(userSearchInput);
+  //getImagesData(userSearchInput);
 });
 //
-
