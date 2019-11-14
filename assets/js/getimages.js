0feff777c2cee7f6f8ecef8824b79a0bca763caf
Tony Saad <tony@wego.com>
2019-11-15 00:55:41 +0100

    Some code review comments

--- assets/js/getimages.js
+++ assets/js/getimages.js
@@ -1,12 +1,12 @@
 var apiKey = "563492ad6f917000010000011d8a4d59e1104cf883c51b9a5999f5ba";
 var randomNumber = Math.floor(Math.random() * 100);
-var queryURL =
-    "https://api.pexels.com/v1/curated?per_page=3&page=" + randomNumber;
+const IMAGES_TRENDING_API = `https://api.pexels.com/v1/curated?per_page=3&page=${randomNumber}`;
+const IMAGES_SEARCH_API = `https://api.pexels.com/v1/search?per_page=3&page=${randomNumber}`;
 
-$(document).ready(function () {
+function fetchImages(url) {
     $.ajax({
         type: "GET",
-        url: queryURL,
+        url: url,
         beforeSend: function (request) {
             request.setRequestHeader("Authorization", apiKey);
         }
@@ -20,28 +20,12 @@ $(document).ready(function () {
         $("#second-slide").attr("src", imageurl2);
         $("#third-slide").attr("src", imageurl3);
     });
-});
+}
 
-
-
+function fetchTrendingImages() {
+    return fetchImages(IMAGES_TRENDING_API);
+}
 
 function getImagesData(searchTerm) {
-    var queryURLSearch =
-        "https://api.pexels.com/v1/search?query=" + searchTerm + "+query&per_page=3&page=" + randomNumber;
-    $.ajax({
-        type: "GET",
-        url: queryURLSearch,
-        beforeSend: function (request) {
-            request.setRequestHeader("Authorization", apiKey);
-        }
-    }).then(function (response) {
-        var imageurl = response.photos[0].src.large;
-        var imageurl2 = response.photos[1].src.large;
-        var imageurl3 = response.photos[2].src.large;
-
-
-        $("#first-slide").attr("src", imageurl);
-        $("#second-slide").attr("src", imageurl2);
-        $("#third-slide").attr("src", imageurl3);
-    });
+    return fetchImages(`${IMAGES_SEARCH_API}&query=${searchTerm}`);
 }
--- assets/js/newsapi.js
+++ assets/js/newsapi.js
@@ -1,24 +1,20 @@
-var keyNews = "7534abe7f3414a5181ffd55cfd6ce0c2";
-var queryURLNews =
-  "https://newsapi.org/v2/top-headlines?sources=mtv-news&apiKey=" + keyNews;
+//I can't use the same variable names like other files ;-), I have to create a new variable
+const NEWS_API_KEY = "7534abe7f3414a5181ffd55cfd6ce0c2";
+const LATEST_HEADLINES_ENDPOINT = `https://newsapi.org/v2/top-headlines?sources=mtv-news&apiKey=${NEWS_API_KEY}`;
+const NEWS_SEARCH_ENDPOINT = `https://newsapi.org/v2/everything?domains=mtv.com,mtv.co.uk&apiKey=${NEWS_API_KEY}`;
 var newsFeed = $("#news-feed");
+const MAX_NEWS_TO_SHOW = 3;
 
-//display the initial content of the news section
-function getNews(response, numberOfArticles) {
-  console.log(response);
-  for (var i = 0; i < numberOfArticles; i++) {
-    var linkImage = response.articles[i].urlToImage;
-    var linkFullArticle = response.articles[i].url;
+//This is a common function, reused for initial load and for search requests too
+function renderNews(articles) {
+  articles.forEach(article => {
+    var linkImage = article.urlToImage;
+    var linkFullArticle = article.url;
     //display news articles with jQuery & Bootstrap
     var articleInfoCard = $("<div>").addClass("row mb-3");
     var articleText = $("<p>")
       .addClass("card-text")
-      .html(
-        "<b>" +
-        response.articles[i].title +
-        "</b><br>" +
-        response.articles[i].description
-      );
+      .html("<b>" + article.title + "</b><br>" + article.description);
 
     var articleTextWrapper = $("<div>").addClass("card-body col-xl-6");
     var articleImage = $("<img>")
@@ -36,51 +32,38 @@ function getNews(response, numberOfArticles) {
     articleInfoCard.append(articleImage);
     articleInfoCard.append(articleTextWrapper);
     newsFeed.append(articleInfoCard);
-  }
+  });
 }
 
 //displaying news mentioning the word/phrase of the user input
 function displayNewsBySearchWord(userInput) {
-  var queryURLuserInputNews =
-    "https://newsapi.org/v2/everything?domains=mtv.com,mtv.co.uk&q=" +
-    userInput +
-    "&apiKey=" +
-    keyNews;
-
+  console.log(`${NEWS_SEARCH_ENDPOINT}&q=${userInput}`);
   $.ajax({
-    url: queryURLuserInputNews,
+    url: `${NEWS_SEARCH_ENDPOINT}&q=${userInput}`,
     method: "GET"
-  }).then(function (response) {
+  }).then(function(response) {
+    //this is a promise ;-)
     newsFeed.empty();
+    $(".no-news").addClass('is-hidden');
     $("#news-title").html(
-      "'" +
-      userInput +
-      "'" +
-      " in the news" +
-      "<i class='fas fa-newspaper'></i>"
+      `'${userInput}' in the news <i class='fas fa-newspaper'></i>`
     );
-    if (response.totalResults == 0) {
-      var noNewsText = $("<p>").addClass("no-news");
-      var noNewsImage = $("<img>")
-        .attr("src", "assets/css/notfound.jpg")
-        .attr("width", "100%");
-      noNewsText.text(
-        "'" +
-        userInput +
-        "' has been under the radar for a while now... Sorry about that! Try again in a few months!"
-      );
-      newsFeed.append(noNewsImage);
-      newsFeed.append(noNewsText);
+    if (!parseInt(response.totalResults) === 0) {
+      $(".no-news").removeClass('is-hidden');
     } else {
-      getNews(response, 3);
+      renderNews(response.articles);
     }
   });
 }
 
 //getting the initial content of the news section
-$.ajax({
-  url: queryURLNews,
-  method: "GET"
-}).then(function (response) {
-  getNews(response, 3);
-});
+//Why not use $(document).ready(function () {... here ?
+function loadInitialNews() {
+  return $.ajax({
+    url: LATEST_HEADLINES_ENDPOINT,
+    method: "GET"
+  }).then(function(response) {
+    //this is a promise ;-)
+    renderNews(response.articles);
+  })
+}
